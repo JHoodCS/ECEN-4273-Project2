b1f6265afa2a88f7356d41ca4a1fd94c08fcedbd
Jacob Hood <JHoodCS@gmail.com>
2025-04-07 14:36:44 -0500

    Added more information

--- image_scraper/temp.py
+++ image_scraper/temp.py
@@ -1,1 +1,4 @@
 # This is just a temp file for the image scraper code ( I am not familiar with web scraping )
+# Use VoTT tool to label images and create a dataset for training.
+# https://medium.com/nerd-for-tech/building-an-object-detector-in-tensorflow-using-bounding-box-regression-2bc13992973f
+# https://pyimagesearch.com/2020/10/12/multi-class-object-detection-and-bounding-box-regression-with-keras-tensorflow-and-deep-learning/
